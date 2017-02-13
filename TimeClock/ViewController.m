2ff72f9a2d80d7a9b9bcc037d4f0fd46e623e8ad
YanPengImp <464553111@qq.com>
2017-02-13 10:38:58 +0800

    Initial Commit

---
+++ TimeClock/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TimeClock
+//
+//  Created by Imp on 17/2/13.
+//  Copyright © 2017年 codoon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
